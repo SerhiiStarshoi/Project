a276478e0d9b4258ae2fc38b296cef1e35f6a99f
Serhii Starshoi <32453285+SerhiiStarshoi@users.noreply.github.com>
2024-09-10 17:43:45 +0300

    Update Main.rb

--- project/Main.rb
+++ project/Main.rb
@@ -24,7 +24,7 @@ location.location_attrs
 expect(location.address).to eq("Budaörs, Akron Utca 2, 2040, Pest, Hungary")
 
 location_updater = LocationManager.new(token)
-updated_location = location_updater.update(create_params, location.location_id)
+updated_location = location_updater.update(create_params, location)
 
 aggregate_failures do
   expect(updated_location.address).to eq("Budaörs, Akron Utca 2, 2040, Pest, Hungary")
@@ -44,7 +44,9 @@ deleted_location.delete(location.location_id)
 # обидва тести як окремі класи
 # апдейт і кріейт один клас +++++
 # назва класу = назва файлу +++++
-# після кожного тесту той локейшен який ми створили він деактивовується (буде в одному ж класі з іншими)
+# після кожного тесту той локейшен який ми створили він деактивовується (буде в одному ж класі з іншими)++++
+# #приймає обєкт локацію, тоді змінна не потрібна ++++
 # постаратись не дублювати код з інших класів (наслідування)
+
 # почитати за cucumber наперед (cucmber ruby! (cucmber.org))
 # rake що таке, і для чого він
