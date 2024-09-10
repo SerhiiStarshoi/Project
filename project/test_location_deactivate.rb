8b702f56b8d1992184b529392a14e6cb5ff62911
Serhii Starshoi <32453285+SerhiiStarshoi@users.noreply.github.com>
2024-09-10 18:42:19 +0300

    Update test_location_deactivate.rb

--- project/test_location_deactivate.rb
+++ project/test_location_deactivate.rb
@@ -16,40 +16,21 @@ create_params = {
   longitude: "18.960501"
 }
 
-update_params = {
-  portal_id: 16288,
-  title: SecureRandom.alphanumeric(11),
-  custom_title: "UPDATED",
-  type: "Standard",
-  latitude: "47.144312",
-  longitude: "21.64162"
-}
-
 location_creator = LocationManager.new(token)
 location = location_creator.create(create_params)
-
-location.location_attrs
-
-expect(location.address).to eq("Budaörs, Akron Utca 2, 2040, Pest, Hungary")
-
-location_updater = LocationManager.new(token)
-updated_location = location_updater.update(update_params, location)
-
-aggregate_failures do
-  expect(updated_location.address).to eq("Budaörs, Akron Utca 2, 2040, Pest, Hungary")
-  expect(updated_location.latitude).to eq(47.14431)
-  expect(updated_location.type).to eq("Standard")
-  puts "End"
-end
 
 deleted_location = LocationManager.new(token)
 deleted_location.delete(location.location_id)
 
+aggregate_failures do
+  
+end
 
 
 
 
-# обидва тести як окремі класи
+
+# обидва тести як окремі класи ++++
 # апдейт і кріейт один клас +++++
 # назва класу = назва файлу +++++
 # після кожного тесту той локейшен який ми створили він деактивовується (буде в одному ж класі з іншими)++++
