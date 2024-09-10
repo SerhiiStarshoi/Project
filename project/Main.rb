e74db148f1d6bd4504d50bf023814e343ec71f19
Serhii Starshoi <32453285+SerhiiStarshoi@users.noreply.github.com>
2024-09-10 17:59:44 +0300

    Update Main.rb

--- project/Main.rb
+++ project/Main.rb
@@ -16,6 +16,15 @@ create_params = {
   longitude: "18.960501"
 }
 
+update_params = {
+  portal_id: 16288,
+  title: SecureRandom.alphanumeric(11),
+  custom_title: "UPDATED",
+  type: "Standard",
+  latitude: "47.144312",
+  longitude: "21.64162"
+}
+
 location_creator = LocationManager.new(token)
 location = location_creator.create(create_params)
 
@@ -24,7 +33,7 @@ location.location_attrs
 expect(location.address).to eq("Budaörs, Akron Utca 2, 2040, Pest, Hungary")
 
 location_updater = LocationManager.new(token)
-updated_location = location_updater.update(create_params, location)
+updated_location = location_updater.update(update_params, location)
 
 aggregate_failures do
   expect(updated_location.address).to eq("Budaörs, Akron Utca 2, 2040, Pest, Hungary")
@@ -34,7 +43,6 @@ aggregate_failures do
 end
 
 deleted_location = LocationManager.new(token)
-
 deleted_location.delete(location.location_id)
 
 
@@ -46,6 +54,7 @@ deleted_location.delete(location.location_id)
 # назва класу = назва файлу +++++
 # після кожного тесту той локейшен який ми створили він деактивовується (буде в одному ж класі з іншими)++++
 # #приймає обєкт локацію, тоді змінна не потрібна ++++
+# захаркоджених значень немає бути #UPDATED ++++++
 # постаратись не дублювати код з інших класів (наслідування)
 
 # почитати за cucumber наперед (cucmber ruby! (cucmber.org))
