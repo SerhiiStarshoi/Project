3d345145f630ad4180f7af5e92a44efc2c036341
SerhiiStarshoi <archjq@gmail.com>
2024-09-27 17:22:37 +0300

    Initial commit

---
+++ project/filerun.rb
@@ -0,0 +1,27 @@
+require_relative 'tests'
+
+what_test = "create"
+
+case what_test
+when "create"
+  Tests::Create.new.whole_test
+when "update"
+  Tests::Update.new.whole_test
+when "delete"
+  Tests::Deactivate.new.whole_test
+else
+  puts "No such test!"
+end
+
+# git_ignore CMD + K
+# видалити перед гіт ігнор файли які були залиті на гіт
+# запушити папку на гіт
+
+# #зробити щоб біфор викликався в усіх тестах але був написати в одному місці ++++
+# get location має +- виглядати як delete (get location сховати ріквести) ++++
+# file_run має мати всі new і ми передаємо стрінгою назву тесту який запускаємо ++++
+# всі рекваєр перенести в модуль ???
+# глянути чи треба інклуд локейшенс ++++
+# $LOAD_PATH? чи можна зробити через рекваєр
+# у всіх методах має бути теж перевірка статусів ++++
+# https://www.atlassian.com/git/tutorials/merging-vs-rebasing
---
+++ project/tests/create.rb
@@ -0,0 +1,34 @@
+require_relative 'base'
+include Locations
+
+module Tests
+  class Create < Base
+    def test_action
+
+      create_params = {
+        portal_id: 16288,
+        title: SecureRandom.alphanumeric(11),
+        custom_title: "some title",
+        type: "Standard",
+        latitude: "47.444956",
+        longitude: "18.960501"
+      }
+
+      puts location_manager.inspect
+
+      location = location_manager.create(create_params)
+
+      puts location.inspect
+
+      expect(location.address).to eq("Budaörs, Akron Utca 2, 2040, Pest, Hungary")
+
+      after_action(location)
+    end
+  end
+end
+
+#Tests::Create.new.whole_test
+
+
+
+
